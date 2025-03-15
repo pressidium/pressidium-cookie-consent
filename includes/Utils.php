29d6d60db218406b136d5fea9bfaaf2c1c32e294
over-engineer <dev@over-engineer.com>
2025-03-16 00:45:49 +0200

    (#106) Add a `Utils::get_color_hex_by_slug()` method That method allows us to retrieve the actual hex code of a color by its slug (useful to work with theme.json color palettes)

--- includes/Utils.php
+++ includes/Utils.php
@@ -156,4 +156,33 @@ class Utils {
         return substr( $string, 0, $length );
     }
 
+    /**
+     * Return the hex code of a color by its slug.
+     *
+     * @param string $color_slug Color slug.
+     *
+     * @return ?string Color hex code.
+     */
+    public static function get_color_hex_by_slug( string $color_slug ): ?string {
+        $palettes = wp_get_global_settings( array( 'color', 'palette' ) );
+
+        if ( empty( $palettes ) ) {
+            return null;
+        }
+
+        foreach ( $palettes as $palette ) {
+            if ( empty( $palette ) ) {
+                return null;
+            }
+
+            foreach ( $palette as $color ) {
+                if ( ! empty( $color['slug'] ) && $color['slug'] === $color_slug ) {
+                    return $color['color'];
+                }
+            }
+        }
+
+        return null;
+    }
+
 }
