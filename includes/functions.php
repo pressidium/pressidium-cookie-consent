47eee0729d0d3c27a80122f8ca236fdcbb553f75
over-engineer <dev@over-engineer.com>
2025-03-16 00:43:20 +0200

    (#106) Simplify the global function `pressidium_cookie_consent_get_cookies()`

--- includes/functions.php
+++ includes/functions.php
@@ -23,31 +23,5 @@ function pressidium_cookie_consent_get_cookies(): array {
 
     $settings = $settings_object->get();
 
-    /*
-     * We use a hardcoded 'en' language since the UI does not support
-     * multiple languages for the cookies list yet.
-     *
-     * In the future, we will need to update this function to accept
-     * a `$language` parameter and return the cookies for the specified
-     * language.
-     */
-    $blocks = $settings['languages']['en']['settings_modal']['blocks'];
-
-    $cookie_categories = array(
-        $blocks[1] ?? array(), // Necessary
-        $blocks[2] ?? array(), // Analytics
-        $blocks[3] ?? array(), // Targeting
-        $blocks[4] ?? array(), // Preferences
-    );
-
-    return array_map(
-        function( $category ) {
-            return array(
-                'title'       => $category['title'] ?? '',
-                'description' => $category['description'] ?? '',
-                'cookies'     => $category['cookie_table'] ?? array(),
-            );
-        },
-        $cookie_categories
-    );
+    return $settings['pressidium_options']['cookie_table'];
 }
